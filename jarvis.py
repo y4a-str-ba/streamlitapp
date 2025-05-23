c638db3c327b12a68b28e67ed3ed1e259b9e6a02
Business Analytics <117344902+y4a-str-ba@users.noreply.github.com>
2025-05-23 16:02:39 +0700

    Update jarvis.py

--- jarvis.py
+++ jarvis.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 st.set_page_config(page_title="Jarvis Dashboard", layout="wide")
 
-df = pd.read_csv("data/dummy_kill_list.csv")
+df = pd.read_csv("data_test_streamlit.csv")
 
 st.title("Jarvis — Kill Search Terms")
 st.write("These terms are identified as low-performing and should be reviewed before being negativized.")
