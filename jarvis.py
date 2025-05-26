659864b032c6721fea438dd77f6f593bb39cf927
Business Analytics <117344902+y4a-str-ba@users.noreply.github.com>
2025-05-26 15:37:52 +0700

    Update jarvis.py
    Signed-off-by: Business Analytics <117344902+y4a-str-ba@users.noreply.github.com>

--- jarvis.py
+++ jarvis.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import hashlib
 import pandas as pd
 import plotly.graph_objects as go
 from google.oauth2.service_account import Credentials
@@ -7,9 +8,45 @@ import gspread
 # Setup page
 st.set_page_config(page_title="Jarvis Dashboard", layout="wide")
 
+# =====================
+# LOGIN PAGE
+# =====================
+if "logged_in" not in st.session_state:
+    st.session_state.logged_in = False
+
+if not st.session_state.logged_in:
+    st.title("🔐 Login to Jarvis Dashboard")
+    username = st.text_input("Username")
+    password = st.text_input("Password", type="password")
+    login_button = st.button("Login")
+
+    # Multi-user credential check
+    users = {
+        "admin": "yes4all123",
+        "hanhbth@yes4all.com": "h@nhBI2025",
+        "duylk@yes4all.com": "duyTeam123",
+        "ngatpth@yes4all.com":"ngat123"
+    }
+
+    if login_button:
+        if username in users and hashlib.sha256(password.encode()).hexdigest() == hashlib.sha256(users[username].encode()).hexdigest():
+            st.session_state.logged_in = True
+            st.session_state.user = username
+            st.experimental_rerun()
+        else:
+            st.error("❌ Invalid username or password")
+    st.stop()
+
+# =====================
+# MAIN DASHBOARD
+# =====================
+st.set_page_config(page_title="Jarvis Dashboard", layout="wide")
+
 # Sidebar filters
 st.sidebar.image("logo.png", width=180)
 st.sidebar.title("Filters")
+
+st.sidebar.markdown(f"👤 Logged in as: **{st.session_state.user}**")
 
 # Set default department to "SFO"
 if "department" not in st.session_state:
