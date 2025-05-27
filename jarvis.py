809eaebb97c84c1c8e2ce8b8cb3336bccd511f5e
Business Analytics <117344902+y4a-str-ba@users.noreply.github.com>
2025-05-27 15:15:25 +0700

    Update jarvis.py
    Signed-off-by: Business Analytics <117344902+y4a-str-ba@users.noreply.github.com>

--- jarvis.py
+++ jarvis.py
@@ -85,7 +85,8 @@ if st.session_state["apply_filters"]:
         ctr_col = pd.to_numeric(df["ctr"], errors="coerce") if "ctr" in df else None
         sales_col = pd.to_numeric(df["sales"], errors="coerce") if "sales" in df else None
 
-        col1, col2, col3, col4, col5 = st.columns(5)
+        col1, col2, col3, col4, col5 = st.columns(4)
+        
         with col1: st.metric("Total Search Terms", len(df))
         with col2: st.metric("Total Campaign Impact","290")
         with col3: st.metric("Estimated Cost Saved", "$10,200")
