1a8d65fc667ddc80426374d559008e3696e7d64a
Business Analytics <117344902+y4a-str-ba@users.noreply.github.com>
2025-06-24 17:07:19 +0700

    Update jarvis_logger.py
    Signed-off-by: Business Analytics <117344902+y4a-str-ba@users.noreply.github.com>

--- jarvis_logger.py
+++ jarvis_logger.py
@@ -1,17 +1,27 @@
 import gspread
 from google.oauth2.service_account import Credentials
 from datetime import datetime
+import pytz
+import pandas as pd
 
 def log_all_terms(edited_df, user, sheet_id, sheet_name, service_account_info):
+
+    edited_df = edited_df.copy()
+    
+    submitted_at = pd.Timestamp.now(tz=pytz.timezone('Asia/Ho_Chi_Minh')).strftime("%Y-%m-%d %H:%M:%S")
+    edited_df["confirmed_by"] = user
+    edited_df["submitted_at"] = submitted_at
+
+    # Google Sheets API
     scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
     creds = Credentials.from_service_account_info(service_account_info, scopes=scope)
     client = gspread.authorize(creds)
-
     sheet = client.open_by_key(sheet_id).worksheet(sheet_name)
 
-    edited_df = edited_df.copy()
-    edited_df["confirmed_by"] = user
-    edited_df["submitted_at"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    # Write Header
+    if not sheet.get_all_values():
+        sheet.append_row(edited_df.columns.tolist(), value_input_option="USER_ENTERED")
 
-    data_to_append = edited_df.astype(str).values.tolist()
-    sheet.append_rows(data_to_append, value_input_option="USER_ENTERED")
+    # Append Rows
+    sheet.append_rows(edited_df.astype(str).values.tolist(), value_input_option="USER_ENTERED")
+
