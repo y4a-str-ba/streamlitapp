4f55bc11e45ed38cc94a722a88736c1ca5a251cb
Business Analytics <117344902+y4a-str-ba@users.noreply.github.com>
2025-07-11 10:50:22 +0700

    Update jarvis.py
    Signed-off-by: Business Analytics <117344902+y4a-str-ba@users.noreply.github.com>

--- jarvis.py
+++ jarvis.py
@@ -348,6 +348,9 @@ with tab1:
     # if selected_filter_reason:
     #     mask_unconfirmed = df_filtered["confirm_from_mkt"] == False
     #     df_filtered.loc[mask_unconfirmed, "reason_category"] = selected_filter_reason
+
+    def force_rerun():
+        st.session_state._rerun = True
         
     edited_df = st.data_editor(
         df_filtered,
