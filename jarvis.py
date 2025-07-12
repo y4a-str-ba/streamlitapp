781283c61caef727dac0ee119a7e18e7dd61aefd
Business Analytics <117344902+y4a-str-ba@users.noreply.github.com>
2025-07-12 21:18:21 +0700

    Update jarvis.py
    Signed-off-by: Business Analytics <117344902+y4a-str-ba@users.noreply.github.com>

--- jarvis.py
+++ jarvis.py
@@ -416,17 +416,10 @@ with tab1:
     df_before_edit = st.session_state.data_editor_df
     newly_unchecked_mask = (df_before_edit["confirm_from_mkt"] == True) & (edited_df["confirm_from_mkt"] == False)
 
-    # if newly_unchecked_mask.any():
-    #     df_to_update = edited_df.copy()
-    #     df_to_update.loc[newly_unchecked_mask, "reason_category"] = st.session_state.selected_filter_reason
-    #     st.session_state.data_editor_df = df_to_update
-    #     st.rerun()
-    # elif not df_before_edit.equals(edited_df):
-    #     st.session_state.data_editor_df = edited_df.copy()
-
     if newly_unchecked_mask.any():
-        edited_df.loc[newly_unchecked_mask, "reason_category"] = st.session_state.selected_filter_reason
-        st.session_state.data_editor_df = edited_df.copy()
+        df_to_update = edited_df.copy()
+        df_to_update.loc[newly_unchecked_mask, "reason_category"] = st.session_state.selected_filter_reason
+        st.session_state.data_editor_df = df_to_update
         st.rerun()
     elif not df_before_edit.equals(edited_df):
         st.session_state.data_editor_df = edited_df.copy()
